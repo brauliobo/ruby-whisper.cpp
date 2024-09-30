63808c4d66662c5d87a036ccb152a17b7333c8ae
Braulio Oliveira <brauliobo@gmail.com>
2024-09-30 19:28:46 -0300

    Bump version 0.2.0

--- lib/whisper.cpp.rb
+++ lib/whisper.cpp.rb
@@ -3,7 +3,8 @@ require_relative 'whisper/audio_processor'
 require_relative 'whisper/model'
 
 module Whisper
-  # The module is intentionally left empty here.
-  # All classes and modules are defined in their respective files.
+
+  VERSION = '0.2.0'
+
 end
 
